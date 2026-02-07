ba5f0ed6bf09bf8c625406ec433d3673b63037a5
bharathmsft1030 <bhramak@microsoft.com>
2026-02-06 22:48:55 -0800

    Enable Next.js static export for Azure Static Web Apps

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // optional if you ever change output folder:
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
